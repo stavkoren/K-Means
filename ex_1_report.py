e72cb113ce7c764b5d058e2441d48f0ce7694589
Stav Koren <stavkoren123@gmail.com>
2020-07-23 10:24:30 +0300

    Initial commit

---
+++ ex_1_report.py
@@ -0,0 +1,76 @@
+# Stav Koren 207128539
+import sys
+from math import sqrt, hypot
+import scipy.io.wavfile
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# the function compute the distance between two points
+def distance_between_two_points(p1x, p1y, p2x, p2y):
+    return hypot(p1x - p2x, p1y - p2y)
+
+
+# the function classifies each point to the nearest centroid
+def classify_point_to_centroid_index(all_points, current_centroids):
+    current_points_centroid = []
+    current_points_centroid_dis = []
+    # for each point
+    for point in all_points:
+        d = list()
+        # calculate distance for point to each centroid
+        for current_centroid in current_centroids:
+            d.append(distance_between_two_points(point[0], point[1], current_centroid[0], current_centroid[1]))
+        # append the index of the nearset centroid
+        current_points_centroid.append(d.index(min(d)))
+        current_points_centroid_dis.append(min(d))
+    return current_points_centroid,current_points_centroid_dis;
+
+
+sample= sys.argv[1]
+fs, y = scipy.io.wavfile.read(sample)
+points = np.array(y.copy())
+centroids = [(1,1),(0,0)]
+k = len(centroids)
+iterations = 0
+dis=0
+old_centroids = []
+distances = []
+f = open("output.txt", "w+")
+while iterations < 10 and not np.array_equal(centroids, old_centroids):
+    # classify each point to centroid
+    points_classifications, points_classifications_dis = classify_point_to_centroid_index(points, centroids)
+    # update centroids
+    old_centroids = centroids.copy()
+    dis=0
+    for centroid_index in range(0, len(centroids)):
+        sum_x = 0
+        sum_y = 0
+        count_points_classify = 0
+        for i in range(0, len(points_classifications)):
+            # if the current point belong to the current centroid
+            if centroid_index == points_classifications[i]:
+                count_points_classify += 1
+                sum_x += points[i][0];
+                sum_y += points[i][1];
+            i += 1
+        if count_points_classify > 0:
+            # calculate new centroid
+            x_mean = round(sum_x / count_points_classify)
+            y_mean = round(sum_y / count_points_classify)
+            centroids[centroid_index]=(x_mean,y_mean)
+        centroid_index += 1
+    print(f"[iter {iterations}]:{','.join([str(j) for j in centroids])}")
+    f.write(f"[iter {iterations}]:{','.join([str(j) for j in centroids])}")
+    f.write("\n")
+    iterations += 1
+    distances.append(sum(points_classifications_dis)/len(points_classifications_dis))
+'''for pdf'''
+#x=[0,1,2,3,4,5,6,7,8,9,10]
+plt.plot(distances, color='g')
+plt.xticks(np.arange(0, 11, 1))
+plt.title("k = " + str(len(centroids)))
+plt.locator_params(axis='x', nbins=10)
+plt.xlabel("iterations")
+plt.ylabel("average distance from centroid")
+plt.show()
